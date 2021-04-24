1630e21c05f44e72ed8f22b4fdd7d132b04f9355
SalvadorSala <48439344+SalvadorSala@users.noreply.github.com>
2021-04-24 12:46:19 +0200

    Add files via upload

---
+++ labels.v000.slp.training_job/inference-script.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sleap-track C:\Data\Videos\C57hp1.avi --frames 96,33,160,131,106,170,172,141,78,79,145,114,53,157,119,56,183,92,61,62 -m 210423_163714.single_instance -o C57hp1.avi.predictions.slp --verbosity json --no-empty-frames
---
+++ labels.v000.slp.training_job/train-script.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sleap-train single_instance.json labels.v000.pkg.slp
